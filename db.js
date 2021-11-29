87160adbf0903958ee4a74c5493308955cc85e6c
johandi <limjohandi@gmail.com>
2021-11-29 11:02:49 +0700

    install package

---
+++ db.js
@@ -0,0 +1,11 @@
+const Pool = require("pg").Pool
+
+const pool = new Pool({
+    host: "ec2-50-16-241-192.compute-1.amazonaws.com",
+    user:"ocswzcfqixoqbc",
+    password:"ae82eca5aa848c2b075ffe9827b04394b6f86e667492294addae8353cc894c52",
+    port:5432,
+    database:"d1hm1nnk6t70i2"
+})
+
+module.exports = pool
---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const apartmentRoute = require("../sqlApi/src/apartment/routes")
+const app = express()
+
+const port = 3000
+
+app.get('/', (req, res)=>{
+    res.send("hello world")
+})
+
+app.use('/api/apartment', apartmentRoute)
+
+app.listen(port, ()=>{console.log( `listening on port ${port}`)})
