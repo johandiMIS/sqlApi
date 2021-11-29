a3e1cf1b42f73ff0867d9af4a67fa02766de9bda
johandi <limjohandi@gmail.com>
2021-11-29 11:52:45 +0700

    ssl auth done

--- db.js
+++ db.js
@@ -1,6 +1,10 @@
 const Pool = require("pg").Pool
 
 const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    },
     host: "ec2-50-16-241-192.compute-1.amazonaws.com",
     user:"ocswzcfqixoqbc",
     password:"ae82eca5aa848c2b075ffe9827b04394b6f86e667492294addae8353cc894c52",
